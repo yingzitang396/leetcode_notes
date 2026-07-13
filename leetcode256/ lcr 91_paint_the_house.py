fc17133853cc4c71e87bf63d3492602cf3b32518
yingzitang396 <tang.yingz@northeastern.com>
2026-07-12 22:08:32 -0700

    update leetcode

--- leetcode256/ lcr 91_paint_the_house.py
+++ leetcode256/ lcr 91_paint_the_house.py
@@ -14,3 +14,15 @@ def minCost(self, costs: List[List[int]]) -> int:
 
 # 建立三个dp表格去比较red， green， blue的最小值
 # 确认i的状态，以i位置结尾，粉刷red，green， blue的总体cost最小
+
+class Solution:
+    def minCost(self, costs: List[List[int]]) -> int:
+        n = len(costs)
+        dp = [[0] * 3 for _ in range(n+1)]  # 创建二维数组
+        for i in range(1, n+1):
+            dp[i][0] = min(dp[i-1][1], dp[i-1][2]) + costs[i-1][0]
+            # 因为dp表加了虚拟节点0使得第一个格子可以进入for循环所以costs二维表里的i需要减少一位
+            dp[i][1] = min(dp[i-1][0], dp[i-1][2]) + costs[i-1][1]
+            dp[i][2] = min(dp[i-1][0], dp[i-1][1]) + costs[i-1][2]
+        return min(dp[n][0], dp[n][1], dp[n][2])
+
