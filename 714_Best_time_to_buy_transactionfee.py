0f8d62d25dccb141b5bd8ba4bbc2409fa38846e1
yingzitang396 <tang.yingz@northeastern.com>
2026-07-19 22:27:17 -0700

    update leetcode

---
+++ 714_Best_time_to_buy_transactionfee.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxProfit(self, prices: List[int], fee: int) -> int:
+        n = len(prices)
+        dp = [[0] * 2 for _ in range(n)]
+        dp[0][0] = -prices[0]
+        for i in range(1, n):
+            dp[i][0] = max(dp[i-1][0], dp[i-1][1] - prices[i])
+            dp[i][1] = max(dp[i-1][1], dp[i-1][0] + prices[i] - fee)
+        return max(dp[n-1][0], dp[n-1][1])
